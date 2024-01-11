4dc5d92f0158ae506a6dbb1a75cda3b02689650a
nabeel nizar <nabeelnizar786@gmail.com>
2024-01-11 09:24:27 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,19 @@
+function backgroundChange(){
+    const colorField = document.querySelector('.color-field')
+    const switcher = document.querySelector('.switcher')
+
+    function newColor(){
+        let red = Math.floor(Math.random() * 256)
+        let green = Math.floor(Math.random() * 256)
+        let blue = Math.floor(Math.random() * 256)
+        return `rgb(${red},${green},${blue})`
+    }
+
+    switcher.addEventListener('click', function(){
+        let color = newColor()
+        document.body.style.background = color
+        colorField.value = color
+    })
+}
+
+backgroundChange()
